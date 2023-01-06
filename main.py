a9f3bfe35303b972c9748c80e916cfd549243b23
TeraByte512 <tbyte256@gmail.com>
2023-01-06 18:36:10 +0200

    Forwarded the HTTP request to the server, forwarded the HTTP request from the server to the external website, sliced the URL from the HTTP request

--- main.py
+++ main.py
@@ -1,70 +1,46 @@
 from socket import *
 import sys
 
-if len(sys.argv) <= 1:
-    print ('Usage : "python ProxyServer.py server_ip"\n[server_ip : It is the IP Address Of Proxy Server')
-    sys.exit(2)
+# Create a TCP/IP socket
+sock = socket(AF_INET, SOCK_STREAM)
 
-# Create a server socket, bind it to a port and start listening
-tcpSerSock = socket(AF_INET, SOCK_STREAM)
+# Bind the socket to the port
+server_address = ('localhost', 8080)
+print('starting up on {} port {}'.format(*server_address))
+sock.bind(server_address)
 
-# Fill in start.
-# Fill in end.
+# Listen for incoming connections
+sock.listen(1)
 
-while 1:
-    # Start receiving data from the client
-    print ('\n\nReady to serve...')
-    tcpCliSock, addr = tcpSerSock.accept()
-    print ('Received a connection from:', addr)
-    message = # Fill in start. # Fill in end.
-    print (message)
-    # Extract the filename from the given message
-    print (message.split()[1])
-    filename = message.split()[1].partition("/")[2]
-    print (filename)
-    fileExist = "false"
-    filetouse = "/" + filename
-    print (filetouse)
+while True:
+    # Wait for a connection
+    print('waiting for a connection')
+    connection, client_address = sock.accept()
     try:
-        # Check wether the file exist in the cache
-        f = open(filetouse[1:], "r")
-        outputdata = f.readlines()
-        fileExist = "true"
-        # ProxyServer finds a cache hit and generates a response message
-        tcpCliSock.send("HTTP/1.0 200 OK\r\n")
-        tcpCliSock.send("Content-Type:text/html\r\n")
-        # Fill in start.
-        # Fill in end.
-        print ('Read from cache')
-        # Error handling for file not found in cache
-    except IOError:
-        if fileExist == "false":
-            # Create a socket on the proxyserver
-            c = # Fill in start. # Fill in end.
-            hostn = filename.replace("www.","",1)
-            print (hostn)
-            try:
-                # Connect to the socket to port 80
-                # Fill in start.
-                # Fill in end.
-                # Create a temporary file on this socket and ask port 80 for the file requested by the client
-                fileobj = c.makefile('r', 0)
-                fileobj.write("GET "+"http://" + filename + " HTTP/1.0\n\n")
-                # Read the response into buffer
-                # Fill in start.
-                # Fill in end.
-                # Create a new file in the cache for the requested file.
-                # Also send the response in the buffer to client socket and the corresponding file in the cache
-                tmpFile = open("./" + filename,"wb")
-                # Fill in start.
-                # Fill in end.
-            except:
-                print ("Illegal request")
-    else:
-        # HTTP response message for file not found
-        # Fill in start.
-        # Fill in end.
-        # Close the client and the server sockets
-        tcpCliSock.close()
-        # Fill in start.
-        # Fill in end
+        print('connection from', client_address)
+
+        # Receive the data in small chunks and retransmit it
+        while True:
+            data = connection.recv(4096)
+            print('received {!r}'.format(data))
+            if data:
+                print('sending data back to the client')
+                # connection.sendall(data)
+
+                # Create a TCP/IP socket
+                external_sock = socket(AF_INET, SOCK_STREAM)
+
+                # Bind the socket to the port
+                server_address = (data.split()[1][5:].decode("utf-8"), 8080)
+                print('starting up on {} port {}'.format(*server_address))
+                external_sock.connect(server_address)
+                external_sock.sendall(data)
+
+                print(external_sock.recv(4096))
+            else:
+                print('no data from', client_address)
+                break
+
+    finally:
+        # Clean up the connection
+        connection.close()
